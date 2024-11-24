9aca5ef0d42b53e605301938067271e32a4be170
Elitesun <elitesun.dev@gmail.com>
2024-11-24 21:50:25 +0000

    trying to deploy

--- netflixclone/build.sh
+++ netflixclone/build.sh
@@ -5,12 +5,6 @@ set -o errexit
 # Install Python dependencies
 pip install -r requirements.txt
 
-Python manage.py collectstatic --no-input
-
-Python manage.py makemigrations
-Python manage.py migrate
-
-
 
 # Run database migrations
 
