83792cbe6c640c04358cb165a520c9ed95fa55ea
Elitesun <elitesun.dev@gmail.com>
2024-11-25 10:18:58 +0000

    sufferness

--- netflixclone/build.sh
+++ netflixclone/build.sh
@@ -5,6 +5,8 @@ set -o errexit
 # Install Python dependencies
 pip install -r requirements.txt
 
+py manage.py collectstatic --no-input
+
 cd netflixclone
 
 
--- netflixclone/netflixclone/settings.py
+++ netflixclone/netflixclone/settings.py
@@ -98,9 +98,12 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files configuration
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static/assets'),
+    ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files configuration
